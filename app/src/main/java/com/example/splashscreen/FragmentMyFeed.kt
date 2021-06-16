2b5f1803047169109d561cba47206212e6539b42
Jackline-ke <antoninahodhiambo@gmail.com>
2021-06-16 21:23:00 +0300

    Api Service

--- app/src/main/java/com/example/splashscreen/FragmentMyFeed.kt
+++ app/src/main/java/com/example/splashscreen/FragmentMyFeed.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.splashscreen.databinding.FragmentHomeBinding
+import com.example.splashscreen.databinding.FragmentMyFeedBinding
 import com.example.splashscreen.databinding.MyBeerRowBinding
 import retrofit2.Call
 import retrofit2.Callback
@@ -15,13 +17,14 @@ private const val TAG = "FragmentMyFeed"
 
 class FragmentMyFeed : Fragment() {
 
-    private lateinit var  binding: MyBeerRowBinding
+    private lateinit var  binding: FragmentMyFeedBinding
+    private val adapter by lazy { BeerAdapter() }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
 
-        binding = MyBeerRowBinding.inflate(inflater,container,false)
+        binding = FragmentMyFeedBinding.inflate(inflater,container,false)
         val view = binding.root
 
         BeersApi.apiService.getBeers().enqueue(object: Callback<Beers>{
@@ -32,6 +35,9 @@ class FragmentMyFeed : Fragment() {
 
             override fun onResponse(call: Call<Beers>, response: Response<Beers>) {
                 Log.d(TAG, "onResponse: ${response.body()}")
+                val myList = response.body()
+                adapter.submitList(myList)
+                binding.recyclerview.adapter = adapter
             }
 
         })
